93a6a528425ee2921b4277af69ce5545871d0299
Esther Seyffarth <esther.seyffarth@gmail.com>
2015-07-03 15:20:41 +0200

    removed dependency on external libraries

--- wikipedia_main.py
+++ wikipedia_main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 __author__ = 'Esther'
@@ -5,7 +6,9 @@ import wikipedia_config
 import tweepy
 import re
 import urllib2
-from wikiapi import WikiApi
+
+import sys
+import time
 
 
 def login():
@@ -24,44 +27,53 @@ def login():
     return api
 
 def stick_together_output():
-    wiki = WikiApi()
-    wiki = WikiApi({ 'locale' : 'en'}) # to specify your locale, 'en' is default
 
-    first_sentence_pattern = re.compile("^(([^\.]+)(\s+is\s+|\s+are\s+|\s+was\s+|\s+were\s+)([^\.]+\.))\s+[A-Z]")
-    bracket_pattern = re.compile("\s+(\(|\[)[^\)\]]+(\)|\])\s+")
+    first_sentence_pattern = re.compile("^(([^\.]+)(\s+is\s+|\s+are\s+|\s+was\s+|\s+were\s+)([^\.]+\.))\s+")
+    round_bracket_pattern = re.compile("\s+(\()[^\)\]]+(\))\s+")
+    square_bracket_pattern = re.compile("\s+(\[)[^\)\]]+(\])\s+")
 
     finished = False
     while not finished:
         seed_page = urllib2.urlopen('https://en.wikipedia.org/wiki/Special:Random')
-        seed_title = seed_page.read().split("<title>")[1].split(" - Wikipedia, the free encyclopedia</title>")[0]
+        seed_content = seed_page.read()
+        seed_title = seed_content.split("<title>")[1].split(" - Wikipedia, the free encyclopedia</title>")[0]
         if "(disambiguation)" not in seed_title and "List of" not in seed_title and "Category:" not in seed_title:
-            results1 = wiki.find(seed_title)
-            article1_content = wiki.get_article(results1[0]).content
-            if article1_content.count(" ") > 2:
-                first_sentence1 = article1_content.split("\n\n")[1]
-                while re.search(bracket_pattern, first_sentence1):
-                    first_sentence1 = re.sub(bracket_pattern, " ", first_sentence1)
-                m = re.findall(first_sentence_pattern, first_sentence1)
-                if m:
-                    first_half = m[0][1]
-                    connector = m[0][2]
-                    output = first_half.strip() +" "+ connector.strip()
-                    second_page_tries = 0
-                    while not finished and second_page_tries < 15:
-                        random_second_page = urllib2.urlopen('https://en.wikipedia.org/wiki/Special:Random')
-                        random_second_page_title = random_second_page.read().split("<title>")[1].split(" - Wikipedia, the free encyclopedia</title>")[0]
-                        second_page_tries += 1
-                        if "(disambiguation)" not in random_second_page_title and "List of" not in random_second_page_title and "Category:" not in random_second_page_title:
-                            results2 = wiki.find(random_second_page_title)
-                            article2_content = wiki.get_article(results2[0]).content
-                            if article2_content.count(" ") > 2:
-                                first_sentence2 = article2_content.split("\n\n")[1].split(". ")[0]+"."
-                                while re.search(bracket_pattern, first_sentence2):
-                                    first_sentence2 = re.sub(bracket_pattern, " ", first_sentence2)
-                                if connector in first_sentence2:
-                                    output = output + " " + first_sentence2[first_sentence2.find(connector)+len(connector):]
-                                    if len(output) < 141:
-                                        finished = True
+
+            seed_text = seed_content.split("<p>")[1].split("</p>")[0]
+            seed_text = re.sub("<[^>]+>", "", seed_text)
+
+            first_sentence1 = re.split("[^\s].\.\s", seed_text, 1)[0]+". "
+
+            while re.search(round_bracket_pattern, first_sentence1):
+                first_sentence1 = re.sub(round_bracket_pattern, " ", first_sentence1)
+            while re.search(square_bracket_pattern, first_sentence1):
+                first_sentence1 = re.sub(square_bracket_pattern, " ", first_sentence1)
+            m = re.findall(first_sentence_pattern, first_sentence1)
+            if m:
+                first_half = m[0][1]
+                connector = m[0][2]
+                output = first_half.strip() +" "+ connector.strip()
+                second_page_tries = 0
+                while not finished and second_page_tries < 15:
+                    random_second_page = urllib2.urlopen('https://en.wikipedia.org/wiki/Special:Random')
+                    random_second_page_content = random_second_page.read()
+                    random_second_page_title = random_second_page_content.split("<title>")[1].split(" - Wikipedia, the free encyclopedia</title>")[0]
+                    second_page_tries += 1
+                    if "(disambiguation)" not in random_second_page_title and "List of" not in random_second_page_title and "Category:" not in random_second_page_title:
+
+                        random_second_page_text = random_second_page_content.split("<p>")[1].split("</p>")[0]
+                        random_second_page_text = re.sub("<[^>]+>", "", random_second_page_text)
+                        first_sentence2 = re.split("[^\s].\.\s", random_second_page_text, 1)[0]+"."
+
+                        while re.search(round_bracket_pattern, first_sentence2):
+                            first_sentence2 = re.sub(round_bracket_pattern, " ", first_sentence2)
+                        while re.search(square_bracket_pattern, first_sentence2):
+                            first_sentence2 = re.sub(square_bracket_pattern, " ", first_sentence2)
+
+                        if connector in first_sentence2:
+                            output = output + " " + first_sentence2[first_sentence2.find(connector)+len(connector):]
+                            if len(output) < 141:
+                                finished = True
 
 
     while output.endswith(".."):
@@ -70,11 +82,13 @@ def stick_together_output():
 
 def tweet_something(debug):
     api = login()
-    output = stick_together_output()
-    if debug:
-        print output
-    else:
-        api.update_status(status=output)
-        print output
-
+    try:
+        output = stick_together_output()
+        if debug:
+            print output
+        else:
+            api.update_status(status=output)
+            print output
+    except:
+        api.send_direct_message(screen_name = "ojahnn", text = str(sys.exc_info())[:139] + " " + time.strftime("%H:%M:%S"))
 tweet_something(False)
